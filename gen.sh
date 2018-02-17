61ace9fc8c5b10cfdc3134c7dd6ce50a892445af
Ganesh Vernekar <cs15btech11018@iith.ac.in>
2018-02-18 00:20:30 +0530

    script to generate test cases

--- gen.sh
+++ gen.sh
@@ -2,23 +2,16 @@
 
 mkdir -p test_cases
 
-echo "Gen 1"
-./build/GenerateGraph 5000 10000 test_cases/batch1.txt
-echo "Gen 2"
-./build/GenerateGraph 6000 12000 test_cases/batch2.txt
-echo "Gen 3"
-./build/GenerateGraph 7000 14000 test_cases/batch3.txt
-echo "Gen 4"
-./build/GenerateGraph 8000 16000 test_cases/batch4.txt
-echo "Gen 5"
-./build/GenerateGraph 9000 18000 test_cases/batch5.txt
-echo "Gen 6"
-./build/GenerateGraph 10000 20000 test_cases/batch6.txt
-echo "Gen 7"
-./build/GenerateGraph 11000 22000 test_cases/batch7.txt
-echo "Gen 8"
-./build/GenerateGraph 12000 24000 test_cases/batch8.txt
-echo "Gen 9"
-./build/GenerateGraph 13000 26000 test_cases/batch9.txt
-echo "Gen 10"
-./build/GenerateGraph 14000 28000 test_cases/batch10.txt
+for i in {1..6}
+do
+	echo "Sparse ${i}e6: ./build/GG ${i}000000 $(( i * 7 )) 0.5 test_cases/sparse_${i}e6.txt" &
+	./build/GG ${i}000000 $(( i * 7 )) 0.5 test_cases/sparse_${i}e6.txt &
+done
+
+for i in {1..6}
+do
+	echo "Dense ${i}e6: ./build/GG ${i}000000 $(( i * 100 )) 0.85 test_cases/dense_${i}e6.txt" &
+	./build/GG ${i}000000 $(( i * 100 )) 0.85 test_cases/dense_${i}e6.txt &
+done
+
+wait
